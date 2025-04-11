d0949218a7c5a49a59f7ea8ba1c83de0b53d3616
Aleksander <kaluskialeksander@gmail.com>
2025-04-11 19:32:55 +0200

    calculate complete

--- statistics/iris_analysis/io/load.py
+++ statistics/iris_analysis/io/load.py
@@ -8,7 +8,7 @@ import os
 import numpy as np
 from pathlib import Path
 
-def load_data(dataset_path:str):
+def load_data(dataset_path:str) -> dict:
     dataset_path = Path(dataset_path)
     assert dataset_path.exists()
     assert dataset_path.is_file()
@@ -23,9 +23,10 @@ def load_data(dataset_path:str):
         # skip the first row
         next(reader, None)
         for row in reader:
-            result["sepal_length"].append(row[0])
-            result["sepal_width"].append(row[1])
-            result["petal_length"].append(row[2])
-            result["petal_width"].append(row[3])
+            result["sepal_length"].append(float(row[0]))
+            result["sepal_width"].append(float(row[1]))
+            result["petal_length"].append(float(row[2]))
+            result["petal_width"].append(float(row[3]))
         return result
 
+print(load_data(r"C:\NYPD_github\NYPD\statistics\iris.csv"))
